f866e1509f2ed7717ef1bb8a74d88eadb54255fc
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2025-06-24 12:04:31 +0100

    Delete src directory

--- src/config/jwt.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    secret: process.env.JWT_SECRET,
-    refreshSecret: process.env.JWT_REFRESH_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN || '15m',
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d'
-};
